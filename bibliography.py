a9097662a6802f7a838b3d9b2ccbfc4405c9821b
Chris Cave <chriscave89@gmail.com>
2019-08-15 20:42:15 +0200

    Fix df append

--- bibliography.py
+++ bibliography.py
@@ -3,10 +3,12 @@
 import sys
 import os
 import pandas as pd
+import numpy as np
+
 
 path_to_bib_dir = "/home/chris/bib"
 bib_columns = ["author", "title", "year", "primary-topic", "secondary-topic", "tags",
-               "last-opened", "frequency", "importance"]
+               "last-opened", "frequency", "importance","location"]
 
 if os.path.isdir(path_to_bib_dir) == False:
     os.makedirs(path_to_bib_dir)
@@ -19,7 +21,7 @@ def create_bib(name_of_bib: str):
     else:
         os.makedirs(path_to_new_bib)
         df = pd.DataFrame(columns=bib_columns)
-        df.to_csv(path_to_new_bib + "/" + name_of_bib + ".csv")
+        df.to_csv(path_to_new_bib + "/" + name_of_bib + ".csv",index=False)
 
 
 if len(sys.argv) == 4 and sys.argv[1] == "create" and sys.argv[2] == "bib":
@@ -32,7 +34,9 @@ def add_to_bib(name_of_bib: str):
         print("No bibliography of that name exists!")
     else:
         df = pd.read_csv(path_to_bib_csv)
-        df.iloc[[0]] = [0,0,0,0,0,0,0,0,0] #todo figure out how to append to a Dataframe and how to retrieve meta-data from a pdf
+        df_add = pd.DataFrame(np.array([[0,0,0,0,0,0,0,0,0]]), columns= bib_columns) #todo figure out how to retrieve meta-data from a pdf
+        df = df.append(df_add)
+        df.to_csv(path_to_bib_csv,index=False)
         print(df)
 
 if len(sys.argv) ==3 and sys.argv[1] == "add":
